02b73ad0e0a7f7e989bb7b3312c5d1e9843f7602
James <James@LAPTOP-6S6RTRCH>
2018-01-31 13:45:44 -0700

    Created 1-31-2018

---
+++ plunder/src/byui/cit260/plunder/model/Player.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.plunder.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author James
+ */
+public class Player implements Serializable {
+    //class instance variables
+    private String name;
+    private double bestTime;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBestTime() {
+        return bestTime;
+    }
+
+    public void setBestTime(double bestTime) {
+        this.bestTime = bestTime;
+    }
+}
