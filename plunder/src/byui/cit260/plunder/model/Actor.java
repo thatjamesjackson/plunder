1ace8f206722454432a7bd69568831fa1fbffb8e
James <James@LAPTOP-6S6RTRCH>
2018-02-01 13:11:05 -0700

    Created 1-31-2018

---
+++ plunder/src/byui/cit260/plunder/model/Actor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.plunder.model;
+
+/**
+ *
+ * @author James
+ */
+public enum Actor {
+    private String name;
+    private String Description;
+    private Point coordinates;
+    private double money;
+}
