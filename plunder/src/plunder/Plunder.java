1c4c171899dfd76480234372944538ca05644705
James <James@LAPTOP-6S6RTRCH>
2018-02-01 14:33:47 -0700

    Created 1-31-2018

--- plunder/src/plunder/Plunder.java
+++ plunder/src/plunder/Plunder.java
@@ -5,6 +5,8 @@
  */
 package plunder;
 
+import byui.cit260.plunder.model.Player;
+
 /**
  *
  * @author James
@@ -15,7 +17,11 @@ public class Plunder {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Player playerOne = new Player();
+        playerOne.setName("John");
+        playerOne.setBestTime(9999.99);
+        
+        System.out.println(playerOne.toString());
     }
     
 }
