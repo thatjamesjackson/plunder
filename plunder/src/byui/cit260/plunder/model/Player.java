3eea35cd8dde7a0edc39a1f6383a3dc8799e6656
James <James@LAPTOP-6S6RTRCH>
2018-02-01 12:45:31 -0700

    Created 1-31-2018

--- plunder/src/byui/cit260/plunder/model/Player.java
+++ plunder/src/byui/cit260/plunder/model/Player.java
@@ -6,6 +6,7 @@
 package byui.cit260.plunder.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
@@ -15,6 +16,40 @@ public class Player implements Serializable {
     //class instance variables
     private String name;
     private double bestTime;
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", bestTime=" + bestTime + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.name);
+        hash = 71 * hash + (int) (Double.doubleToLongBits(this.bestTime) ^ (Double.doubleToLongBits(this.bestTime) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (Double.doubleToLongBits(this.bestTime) != Double.doubleToLongBits(other.bestTime)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
 
     public String getName() {
         return name;
