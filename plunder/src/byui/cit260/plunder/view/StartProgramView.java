2a53dc2385f029f5de221554feb014c6755c5912
James <James@LAPTOP-6S6RTRCH>
2018-02-21 13:47:59 -0700

    2-21-2018 added view layer

---
+++ plunder/src/byui/cit260/plunder/view/StartProgramView.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.plunder.view;
+
+/**
+ *
+ * @author James
+ */
+public class StartProgramView {
+    public void display() { 
+ boolean endView = false; 
+ do{
+inputs = getInputs() ;
+if (no inputs OR first input is Q);
+ return;
+endView = doAction(inputs);
+    } while(endView != true);
+}
+
+    public void displayStartProgramView() {
+
+    }
+
+    public StartProgramView() {
+    }
+}
