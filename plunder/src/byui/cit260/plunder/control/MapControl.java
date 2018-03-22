bac86733968e37e12a9ad06c41225cddc1a4f25c
doubleomidge <abigailking@10.38.166.125>
2018-03-22 13:21:26 -0600

    3/22/2017 - resolve issues with Map Control

--- plunder/src/byui/cit260/plunder/control/MapControl.java
+++ plunder/src/byui/cit260/plunder/control/MapControl.java
@@ -91,42 +91,42 @@ public class MapControl {
         islandResourceScene.setDescription("While you are on the island, you see some useful items here.");
         islandResourceScene.setResource(items.get(InventoryItemType.coconut.ordinal()));
         scenes[SceneType.islandResource.ordinal()] = islandResourceScene;
-        
+
         CombatScene combatFBoat = new CombatScene();
         combatFBoat.setDescription("A fishing vessel lies over yonder");
         combatFBoat.setOpponent(CombatControl.enemyShipConstructor(ShipType.fishingBoat.ordinal()));
         scenes[SceneType.fishingBoat.ordinal()] = combatFBoat;
-        
+
         CombatScene combatSBoat = new CombatScene();
         combatSBoat.setDescription("A sailing boat aproaches");
-       combatSBoat.setOpponent(CombatControl.enemyShipConstructor(ShipType.sailBoat.ordinal()));
+        combatSBoat.setOpponent(CombatControl.enemyShipConstructor(ShipType.sailBoat.ordinal()));
         scenes[SceneType.sailBoat.ordinal()] = combatSBoat;
-        
+
         CombatScene combatClipper = new CombatScene();
         combatClipper.setDescription("A clipper speeds across the water");
-       combatClipper.setOpponent(CombatControl.enemyShipConstructor(ShipType.clipper.ordinal()));
+        combatClipper.setOpponent(CombatControl.enemyShipConstructor(ShipType.clipper.ordinal()));
         scenes[SceneType.clipper.ordinal()] = combatClipper;
-        
+
         CombatScene combatFrigate = new CombatScene();
         combatFrigate.setDescription("A frigate aproaches and fires a warning shot");
-       combatFrigate.setOpponent(CombatControl.enemyShipConstructor(ShipType.frigate.ordinal()));
+        combatFrigate.setOpponent(CombatControl.enemyShipConstructor(ShipType.frigate.ordinal()));
         scenes[SceneType.frigate.ordinal()] = combatFrigate;
-        
+
         CombatScene combatGunBoat = new CombatScene();
         combatGunBoat.setDescription("A Gunboat sets course for you");
-       combatGunBoat.setOpponent(CombatControl.enemyShipConstructor(ShipType.gunBoat.ordinal()));
+        combatGunBoat.setOpponent(CombatControl.enemyShipConstructor(ShipType.gunBoat.ordinal()));
         scenes[SceneType.gunBoat.ordinal()] = combatGunBoat;
-        
+
         CombatScene combatManOfWar = new CombatScene();
         combatManOfWar.setDescription("A Man of War has ye in it's sights, prepare for a tough fight");
-       combatManOfWar.setOpponent(CombatControl.enemyShipConstructor(ShipType.manOfWar.ordinal()));
+        combatManOfWar.setOpponent(CombatControl.enemyShipConstructor(ShipType.manOfWar.ordinal()));
         scenes[SceneType.manOfWar.ordinal()] = combatManOfWar;
-        
+
         CombatScene combatSeaMonster = new CombatScene();
         combatSeaMonster.setDescription("From the depths you see a terrifying sea monster rise and open its jaws.");
         combatSeaMonster.setOpponent(CombatControl.enemyShipConstructor(ShipType.seaMonster.ordinal()));
         scenes[SceneType.seaMonster.ordinal()] = combatSeaMonster;
-        
+
         ShopScene shopIsland = new ShopScene();
         shopIsland.setDescription("A welcoming merchant offers ye up a trade.");
         // shopIsland.setUpgrade();
@@ -136,7 +136,7 @@ public class MapControl {
         treasure.setDescription("Glittering gold, sparkling jewels, and heavy too. For a pirate, this be the most beautiful sight in the world.");
         treasure.setResource(items.get(InventoryItemType.jewels.ordinal()));
         scenes[SceneType.treasure.ordinal()] = treasure;
-        
+
         ResourceScene oceanResource = new ResourceScene();
         oceanResource.setDescription("There be many fish in these waters");
         oceanResource.setResource(items.get(InventoryItemType.fish.ordinal()));
@@ -152,53 +152,53 @@ public class MapControl {
 
         return scenes;
     }
+
     private static DecisionScene[] createQuestions() {
         // System.out.println("createQuestions called");
-        
+
         DecisionScene[] choices = new DecisionScene[6];
-       
+
         DecisionScene explore = new DecisionScene();
         explore.setAsk("Do you want to keep exploring?");
         explore.setOptions("Yea or nae?");
         choices[QuestionType.keepExploring.ordinal()] = explore;
-        
+
         DecisionScene dig = new DecisionScene();
         dig.setAsk("Want to dig and try to find some items?");
         dig.setOptions("Yea or nae?");
         choices[QuestionType.dig.ordinal()] = dig;
-        
+
         DecisionScene bribeCombat = new DecisionScene();
         bribeCombat.setAsk("Would ye rather bribe them in hopes they won't attack?");
         bribeCombat.setOptions("Yea or nae?");
         choices[QuestionType.bribe.ordinal()] = bribeCombat;
-        
+
         DecisionScene engageCombat = new DecisionScene();
         engageCombat.setAsk("Want to blow it out of the sea?");
         engageCombat.setOptions("Aye aye or nae?");
         choices[QuestionType.engage.ordinal()] = engageCombat;
-        
+
         DecisionScene runCombat = new DecisionScene();
         runCombat.setAsk("Would ye rather run from this fight?");
         runCombat.setOptions("Yea or nae?");
         choices[QuestionType.runAway.ordinal()] = runCombat;
-        
+
         DecisionScene harvestMaterial = new DecisionScene();
         harvestMaterial.setAsk("Is this worth keeping? Want to take it?");
         harvestMaterial.setOptions("Take it or leave it?");
         choices[QuestionType.harvest.ordinal()] = harvestMaterial;
-        
+
         return choices;
     }
 
     private static void assignQuestionsToScenes(DecisionScene[] choices, RegularScene[] scenes) {
         // System.out.println("assignQuestionsToScenes called");
-        
+
         // Assign questions to the first question scene
         DecisionScene islandRegularScene = (DecisionScene) scenes[SceneType.islandRegular.ordinal()];
-            islandRegularScene.setDecision(choices[QuestionType.dig.ordinal()]);
-        
-       }
+        islandRegularScene.setDecision(choices[QuestionType.dig.ordinal()]);
 
+    }
 
     private static void assignItemsToScenes(ArrayList items, RegularScene[] scenes) {
         System.out.println("assignItemsToScenes called");
@@ -230,9 +230,7 @@ public class MapControl {
         locations[2][4].setScene(scenes[SceneType.frigate.ordinal()]);
         locations[3][4].setScene(scenes[SceneType.seaMonster.ordinal()]);
         locations[4][4].setScene(scenes[SceneType.treasure.ordinal()]);
-        
 
-        
     }
 
 }
