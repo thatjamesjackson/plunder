11b55eb3c2153fd5f41ac830716f4fea4b032173
James <James@LAPTOP-6S6RTRCH>
2018-04-09 12:50:36 -0600

    4/9/2018: drop view and inventory view work

--- plunder/src/byui/cit260/plunder/view/DropView.java
+++ plunder/src/byui/cit260/plunder/view/DropView.java
@@ -6,6 +6,8 @@
 package byui.cit260.plunder.view;
 import byui.cit260.plunder.control.InventoryControl;
 import byui.cit260.plunder.model.InventoryItem;
+import java.util.ArrayList;
+import plunder.Plunder;
 
 /**
  *
@@ -28,7 +30,6 @@ public class DropView extends View {
                 + "================================ \n"
                 + "        Do ye want to:\n"
                 + "   A  - Drop all of an item\n"
-                + "   H  - Drop half of an item\n"
                 + "   P  - Drop part of an item\n"
                 + "   Q  - Quit \n");
         return inputs;
@@ -38,18 +39,15 @@ public class DropView extends View {
     public boolean doAction(String[] inputs) {
         // NPC[] crew = getCrew();
         // getCrewMenu(crew);
-        InventoryItem yerInventory = new InventoryItem
-
+        ArrayList<InventoryItem> yerInventory = Plunder.getCurrentGame().getInventory();
         inputs[0] = inputs[0].toUpperCase().trim();
+        
+        
         switch (inputs[0]) {
             case "A":
                 allDrop(yerInventory);
                 break;
 
-            case "H":
-                halfDrop(yerInventory);
-                break;
-                
             case "P":
                 partDrop(yerInventory);
 
@@ -61,16 +59,25 @@ public class DropView extends View {
         return true;
     }
 
-    private void allDrop(InventoryItem yerInventory) {
-        System.out.println("Which item do ye want to get all rid of?");
+    private void allDrop(ArrayList<InventoryItem> inventory) {
+        InventoryView inv = new InventoryView();
+        inv.listInventory();
+        String input = getInput("Which item do ye want to get all rid of?");
+        for(InventoryItem curItem: inventory){
+            if(curItem.getTypeAbr().equals(input)){
+            curItem.setQuanti
+            }
+            
+        }
     }
 
-    private void halfDrop(InventoryItem yerInventory) {
-        System.out.println("Which item do ye want to get rid of half yer portion?");
-    }
 
-    private void partDrop(InventoryItem yerInventory) {
+
+    private void partDrop(ArrayList<InventoryItem> yerInventory) {
         System.out.println("What item do ye want to drop a certain part of?");
+        InventoryView inv = new InventoryView();
+        inv.listInventory();
+        
         
 //        System.out.println("How much of " + inventoryitem + " do ye want to drop?");
     }
